cd36d1da34e10de80e046ef2a1246bf49fc1b920
aryank-prog <70921613+aryank-prog@users.noreply.github.com>
2022-12-28 06:13:38 +0000

    Add files via upload
    home page finished, about page basically finished

---
+++ js/about.js
@@ -0,0 +1,39 @@
+// const targetDiv1 = document.getElementById("workID");
+// const btn1 = document.getElementById("work");
+
+
+// const targetDiv2 = document.getElementById("orgsID");
+// const btn2 = document.getElementById("orgs");
+
+// const targetDiv3 = document.getElementById("danceID");
+// const btn3 = document.getElementById("dance");
+
+// btn1.onclick = function () {
+//   if (targetDiv1.style.visibility == "hidden") {
+//     targetDiv1.style.visibility = "visible";
+//   } else {
+//     targetDiv1.style.visibility = "hidden";
+//   }
+// };
+
+
+// btn2.onclick = function () {
+//   if (targetDiv2.style.visibility == "hidden") {
+//     targetDiv2.style.visibility = "visible";
+
+
+//   } else {
+//     targetDiv2.style.visibility = "hidden";
+//   }
+// };
+
+
+// btn3.onclick = function () {
+//   if (targetDiv3.style.visibility == "hidden") {
+//     targetDiv3.style.visibility = "visible";
+
+
+//   } else {
+//     targetDiv3.style.visibility = "hidden";
+//   }
+// };
